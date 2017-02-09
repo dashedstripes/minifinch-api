de6935e7b4dfb2247028c20489414a89b784a044
Adam Gray <adam@pateo.co>
2017-02-09 20:13:13 +0000

    Add socket.io

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const PORT = process.env.PORT || 3030
 const api_router = express.Router()
 
-app.use(cors())
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5100')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  next()
+})
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+app.options('*', cors())
 
 require('./routes/setup')(api_router)
 require('./routes/accounts')(api_router)
@@ -15,10 +26,14 @@ require('./routes/minifinch')(api_router)
 
 app.use('/api', api_router)
 
+io.on('connection', (socket) => {
+  console.log('connected')
+})
+
 app.get('*', (req, res) => {
   res.send('')
 })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`API Server listening on port ${PORT}`)
 })
